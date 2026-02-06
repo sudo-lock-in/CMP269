11ee36cc4ab29a546b11aafe644d87b13f36ee31
A T <amythompson0325@gmail.com>
2026-02-06 08:48:24 -0500

    fix up exercise 1

--- Feb5th/Exercise 1/Driver.java
+++ Feb5th/Exercise 1/Driver.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 public class Driver {
     public static void main(String args[]) {
+        //Payable is an interface, the arraylist has to be PaymentMethod
        ArrayList<PaymentMethod> paymentQueue = new ArrayList<>();
        CreditCard card = new CreditCard("Arthur", 500.0, 1000.0);
        MealPlan plan = new MealPlan("Jungkook", 1000.0);
@@ -11,7 +12,6 @@ public class Driver {
        for (int i = 0; i < paymentQueue.size(); i++) {
            paymentQueue.get(i).processPayment(50.0);
        }
-      System.out.println(CreditCard.getTotalTransactions());
-      System.out.println(MealPlan.getTotalTransactions());
+      System.out.println(PaymentMethod.getTotalTransactions());
     }
 }
