ab07149b1ce6ab1fbda3e0a5da5f7d0c83c04f03
A T <amythompson0325@gmail.com>
2026-02-06 14:29:28 -0500

    finish smartthermostat

--- Feb5th/Exercise 2/SmartThermostat.java
+++ Feb5th/Exercise 2/SmartThermostat.java
@@ -1,11 +1,45 @@
 public class SmartThermostat extends SmartDevice implements Adjustable {
+    private int temperature;
+
+    public SmartThermostat(String deviceName) {
+        super(deviceName);
+    }
+
+    
+
     @Override
-    public void performSelfDiagnostic() {
+    public void setLevel(int level) {
+        if (!isOn) {
+            System.out.println("Cannot adjust: Device is OFF.");
+        } else {
+            if (level < 60 || level > 80) {
+                throw new IllegalArgumentException("Temperature level must be between 60 and 80.");
+            }
+            this.temperature = level;
+        }
     }
 
     @Override
-    public void turnOff() {
+    public void performSelfDiagnostic() { //i need to implement this method because it is abstract in the parent class
+        System.out.println("Checking temperature sensor...");
+    }
 
+    //abstract methods cannot be called with super. i would have to extend from smartlight
+    @Override
+    public void turnOff() {
+        if (isOn) {
+            isOn = false;
+            activeDevicesCount--;
+        }
+    }   
+
+    @Override
+    public void turnOn() {
+        System.out.println("HVAC System Starting...");
+        if (!isOn) {
+            isOn = true;
+            activeDevicesCount++;
+        }
     }
 
 
