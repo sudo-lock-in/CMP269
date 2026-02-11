2b851afc2c72ecc6957c5f6fb78e4fe40fd86c9b
A T <amythompson0325@gmail.com>
2026-02-10 22:30:38 -0500

    exercise complete

--- Exercise 3/Processor.java
+++ Exercise 3/Processor.java
@@ -5,32 +5,29 @@ public class Processor {
     public static void main(String[] args) {
         FileInputStream fileIS;
         Scanner fileScan;
-        String lineContent = "";
+        String lineContent;
+        PrintWriter output;
+        Scanner inStream;
          try {
                 fileIS = new FileInputStream("students.txt");
                 fileScan = new Scanner(fileIS);
-                Scanner inStream = null;
                 inStream = new Scanner(new File("students.txt"));
+                output = new PrintWriter(new FileOutputStream("grades_report.txt", true));
                 while (inStream.hasNextLine()) {
-
                     lineContent = inStream.nextLine();
                     String[] content = lineContent.split(" ");
                     try {
-                        String name = content[0];
+                        String name = content[0].replace("_", " ");
                         int score1 = Integer.parseInt(content[1]);
                         int score2 = Integer.parseInt(content[2]); 
                         int score3 = Integer.parseInt(content[3]); 
                         int average = (score1 + score2 + score3)/3;
-                        PrintWriter output = new PrintWriter(new FileOutputStream("grades_report.txt", true));
                         output.println("Student: " + name + " | Average: " + average);
                         output.flush(); 
-                        
                     }
-                    catch (InputMismatchException e) {
+                    catch (NumberFormatException e) {
                         System.out.println("Invalid input. Student skipped.");
-                        continue;
                     }
-
                 }
             } catch (FileNotFoundException e) {
             } 
