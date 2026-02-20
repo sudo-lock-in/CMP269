500ff2b37966b80ee431d95215c8a2150cc15094
A T <amythompson0325@gmail.com>
2026-02-20 14:22:45 -0500

    added labels, buttons, and fields to grid

---
+++ Exercise4/RegistrationApp.java
@@ -0,0 +1,43 @@
+package Exercise4;
+
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class RegistrationApp extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(20));
+        grid.setVgap(10);
+        grid.setHgap(10);
+        
+        TextField nameField = new TextField();
+        Label nameLabel = new Label("Student Name");
+        grid.add(nameLabel, 0, 0);
+        grid.add(nameField, 1, 0);
+        TextField courseField = new TextField();
+        Label courseLabel = new Label("Course Code");
+        Label status = new Label("Status: Incomplete");
+        Button register = new Button("Register");
+        grid.add(courseLabel, 2, 0);
+        grid.add(courseField, 3, 0);
+        grid.add(status, 4, 0);
+        grid.add(register, 5, 0);
+        
+
+        Scene scene = new Scene(grid, 400, 250);
+        primaryStage.setTitle("Lehman Course Registration");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
