aa414d26a77950810db3cd5da91afd744f751e55
A T <amythompson0325@gmail.com>
2026-03-18 17:53:53 -0400

    part 2 complete

--- Kotlin/Exercise1/Exercise2.kt
+++ Kotlin/Exercise1/Exercise2.kt
@@ -1,6 +1,17 @@
 data class Laptop(val brand: String, val ramGB: Int)
 
+fun Int.toLehmanGigabytes(round: Boolean = false): String {
+    if (this == 16) {
+        return "$this GB (Lehman Standrd)"
+    } else {
+        return "$this GB (Not Lehman Standrd)"
+    }
+}
+
 fun main() {
     val laptop1 = Laptop("Lenovo", 32)
-        val laptop2 = Laptop("HP", 8)
+    val laptop2 = Laptop("HP", 16)
+    println(laptop1.ramGB.toLehmanGigabytes())
+    println(laptop2.ramGB.toLehmanGigabytes())
+
 }
