bfd1852ed6c94987ec53e4225dca497d1836e9fd
A T <amythompson0325@gmail.com>
2026-03-20 22:59:03 -0400

    save progress

---
+++ Kotlin/Exercise2/WebResponse.kt
@@ -0,0 +1,10 @@
+package Exercise2
+
+data class WebResponse(val statusCode: Int, val statusMessage: String, val body: String?)
+
+fun main() {
+    val success = WebResponse(200, "success", null)
+    val notFound = WebResponse(404, "not-found", null)
+    println(notFound)
+    println(success)
+}
