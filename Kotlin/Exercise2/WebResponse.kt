0a066d88830a685ead34a68923cb60a883fc0f4d
A T <amythompson0325@gmail.com>
2026-03-21 20:52:07 -0400

    kotlin exercise 2 complete

--- Kotlin/Exercise2/WebResponse.kt
+++ Kotlin/Exercise2/WebResponse.kt
@@ -8,18 +8,39 @@ fun main() {
     val notFound = WebResponse(404, "not-found", null)
     println(notFound)
     println(success)
+    println()
 
-    describeStatus(success.statusCode) //exercise 2
-    describeStatus(notFound.statusCode)
+    //exercise 2
+    println(describeStatus(success.statusCode))
+    println(describeStatus(notFound.statusCode))
+    println()
+
+    //exercise 3
+    println(routeRequest("/home", "Alice"))
+    println(routeRequest("/grades", "Bob"))
+    println(routeRequest("/unknown", null))
+    println(routeRequest("/grades", null))
+
 }
 
 // exercise 2
-fun describeStatus(code: Int) {
-    when (code) {
-        in 200..299 -> println("Success: The request was fulfilled.")
-        in 400..499 -> println("Client Error: Check your URL or parameters.")
-        in 500..599 -> println("Server Error: The Lehman Server is having trouble.")
-        else -> println("Unknown status code.")
+fun describeStatus(code: Int): String {
+    return when (code) {
+        in 200..299 -> "Success: The request was fulfilled."
+        in 400..499 -> "Client Error: Check your URL or parameters."
+        in 500..599 -> "Server Error: The Lehman Server is having trouble."
+        else -> "Unknown status code."
     }
 }
 
+//exercise 3
+fun routeRequest(path: String, user: String?): String {
+    return when (path) {
+        in "/home" -> "Welcome to the Lehman Homepage, ${user ?: "Guest"}!"
+        in "/grades" -> {
+            val userExist = user ?: return "Error: Unauthorized access to grades."
+            "Loading grades for $userExist..."
+        }
+        else -> "404: Path $path not found."
+    }
+}
