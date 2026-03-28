54b65d0c76194879b5bc79292e06b8ffc8a7899a
A T <amythompson0325@gmail.com>
2026-03-28 09:50:09 -0400

    save progress

--- Kotlin/Exercise3/ktor/src/main/kotlin/Application.kt
+++ Kotlin/Exercise3/ktor/src/main/kotlin/Application.kt
@@ -1,11 +1,22 @@
 package com.example
 
 import io.ktor.server.application.*
-
-fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
-}
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun Application.module() {
-    configureRouting()
+    routing {
+        get("/") {
+            call.respondText("Server is online at Lehman College.")
+        }
+        get("/greet/{name}") {
+            val name = call.parameters["name"]
+            call.respondText("Hello, $name! Welcome to CMP 269.")
+        }
+        get("/greet/{name}") {
+            val name = call.parameters["name"]
+            call.respondText("Hello, $name! Welcome to CMP 269.")
+        }
+    }
 }
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
