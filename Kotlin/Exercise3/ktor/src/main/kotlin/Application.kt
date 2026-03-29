11a1f21a1e8093f9f5b873dd001feb7ddbabcf9d
A T <amythompson0325@gmail.com>
2026-03-28 21:45:48 -0400

    save progress

--- Kotlin/Exercise3/ktor/src/main/kotlin/Application.kt
+++ Kotlin/Exercise3/ktor/src/main/kotlin/Application.kt
@@ -3,6 +3,9 @@ package com.example
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.http.*
+
+val grades = mapOf("123" to 95, "456" to 82)
 
 fun Application.module() {
     routing {
@@ -13,10 +16,11 @@ fun Application.module() {
             val name = call.parameters["name"]
             call.respondText("Hello, $name! Welcome to CMP 269.")
         }
-        get("/greet/{name}") {
-            val name = call.parameters["name"]
-            call.respondText("Hello, $name! Welcome to CMP 269.")
+        get("/grade/{studentId}") {
+            val studentId = call.parameters["studentId"]
+            val grade = grades[studentId] ?: return@get call.respondText("Student not found.", status = HttpStatusCode.NotFound)
+            call.respondText("Student $studentId earned a grade of $grade.")
+            }
         }
     }
-}
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
