c1f68b7f862fd5f78dfcd75ab664279941740f4c
A T <amythompson0325@gmail.com>
2026-04-05 19:01:12 -0400

    save progress

--- Kotlin/Exercise4/ktor/src/main/kotlin/Application.kt
+++ Kotlin/Exercise4/ktor/src/main/kotlin/Application.kt
@@ -4,6 +4,8 @@ import com.google.zxing.BarcodeFormat
 import com.google.zxing.client.j2se.MatrixToImageWriter
 import com.google.zxing.qrcode.QRCodeWriter
 import io.ktor.server.application.*
+import java.io.ByteArrayOutputStream
+import java.io.OutputStream
 import java.nio.file.Path
 
 fun main(args: Array<String>) {
@@ -11,11 +13,16 @@ fun main(args: Array<String>) {
 }
 
 fun Application.module() {
-    saveQRCode("amy.thompson@lc.cuny.edu", "my_email.png")
+    saveQRCode("amy.thompson@lc.cuny.edu", "my_email.png") // part 1
     configureRouting()
 }
-
+// part 1
 fun saveQRCode(content: String, fileName: String) {
     val bitMatrix = QRCodeWriter().encode(content, BarcodeFormat.QR_CODE, 300, 300)
     MatrixToImageWriter.writeToPath(bitMatrix, "PNG", Path.of(fileName))
+}
+// part 2
+fun saveQRCodeStream(content: String, outputStream: OutputStream) {
+    val bitMatrix = QRCodeWriter().encode(content, BarcodeFormat.QR_CODE, 300, 300)
+    MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream)
 }
